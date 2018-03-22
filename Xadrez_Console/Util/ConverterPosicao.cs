49206f69f7c99ef0c55f4d6553aba5251b69acc3
Tiago <tiagorocha2@hotmail.com>
2018-03-22 09:51:19 -0300

    create virtual repository

---
+++ Xadrez_Console/Util/ConverterPosicao.cs
@@ -0,0 +1,31 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xadrez_Console.Util
+{
+    public class ConverterPosicao
+    {
+        public char coluna { get; private set; }
+        public int linha { get; private set; }
+
+       public ConverterPosicao(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+
+        }
+
+        public Posicao toPosicao() {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+
+    }
+}
